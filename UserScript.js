492ed93f6453e0e00239d5212335afd26656b9c1
joeycorbett <corbettjoey9@gmail.com>
2024-03-21 12:37:52 -0400

    Add tampermonkey header and boilerplate code
    Will import required libaries and set download/update url

---
+++ UserScript.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         IMS+ V2
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Provides functionality to IMS 
+// @author       JoeyCorbett
+// @author       TylerMong
+// @match        https://ims.stockton.edu/*
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@10
+// @require      https://kit.fontawesome.com/f54d2a88b1.js
+// @downloadURL  https://github.com/JoeyCorbett/IMS-PLUS/blob/main/Main%20Script.js
+// @updateURL    https://github.com/JoeyCorbett/IMS-PLUS/blob/main/Main%20Script.js
+// @grant        none
+// ==/UserScript==
+
+// SHORTCUTS
+
+// GUI
+
+// Visual Tweaks
