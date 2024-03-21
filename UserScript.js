b2f0ea24743f5a66e6c976df3c23b4e4fc2fc528
joeycorbett <corbettjoey9@gmail.com>
2024-03-21 14:16:32 -0400

    Add shortcuts to script
    Shortcuts include Resolve and Reopen ticket, Comment box selection, and Account button selection

--- UserScript.js
+++ UserScript.js
@@ -14,6 +14,56 @@
 // ==/UserScript==
 
 // SHORTCUTS
+(function ()
+{
+    'use strict'
+
+    function resolveAndReopenTicket(event)
+    {
+        if (event.ctrlKey && event.key === 'Enter')
+        {
+            const ResolveButton = document.querySelectorAll('button.mantine-focus-never.mantine-active.m-77c9d27d.mantine-Button-root.m-87cf2631.mantine-UnstyledButton-root');
+            ResolveButton.forEach(button =>
+            {
+                button.click();
+            });
+        }
+        if (event.ctrlKey && event.key === 'Enter')
+        {
+            const ReOpenButton = document.querySelectorAll('button.mantine-focus-never.mantine-active.m-77c9d27d.mantine-Button-root.m-87cf2631.mantine-UnstyledButton-root');
+            ReOpenButton.forEach(button =>
+            {
+                button.click();
+            });
+        }
+    }
+
+    function selectCommentBox(event)
+    {
+        if (event.ctrlKey && event.key === '\\')
+        {
+            // Dark Mode
+            const textarea_dark = document.querySelector('.m-8fb7ebe7.mantine-Input-input.mantine-Textarea-input');
+            if (textarea_dark)
+                textarea_dark.select();
+        }
+    }
+
+    function SelectAccounts(event)
+    {
+        if (event.altKey && event.key === 'a')
+        {
+            const Accounts_button = document.querySelector('a[href="/ng/accounts"].mantine-focus-never.i-root-iHDUF.m-f0824112.mantine-NavLink-root.m-87cf2631.mantine-UnstyledButton-root');
+            if (Accounts_button)
+                Accounts_button.click();
+        }
+    }
+
+    document.addEventListener('keydown', resolveAndReopenTicket);
+    document.addEventListener('keydown', selectCommentBox);
+    document.addEventListener('keydown', SelectAccounts);
+})();
+
 
 // GUI
 
