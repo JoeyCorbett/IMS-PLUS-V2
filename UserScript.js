44014cc61de6223d29249201831b7581a82adc46
JoeyCorbett <corbettjoey9@gmail.com>
2024-03-22 14:25:42 -0400

    Fix code formatting and change resolve/repopen function

--- UserScript.js
+++ UserScript.js
@@ -14,62 +14,55 @@
 // ==/UserScript==
 
 // SHORTCUTS
-(function ()
-{
+(function () {
     'use strict'
 
-    function resolveAndReopenTicket(event)
-    {
-        if (event.ctrlKey && event.key === 'Enter')
-        {
-            const ResolveButton = document.querySelectorAll('button.mantine-focus-never.mantine-active.m-77c9d27d.mantine-Button-root.m-87cf2631.mantine-UnstyledButton-root');
-            ResolveButton.forEach(button =>
-            {
-                button.click();
-            });
+    function resolveAndReopenTicket(event) {
+        if (event.ctrlKey && event.key === 'Enter') {
+            const ResolveButton = document.querySelector('button.mantine-focus-never.mantine-active.m-77c9d27d.mantine-Button-root.m-87cf2631.mantine-UnstyledButton-root');
+            if (ResolveButton) {
+                ResolveButton.click();
+            }
+            
         }
-        if (event.ctrlKey && event.key === 'Enter')
-        {
+        if (event.ctrlKey && event.key === 'Enter') {
             const ReOpenButton = document.querySelectorAll('button.mantine-focus-never.mantine-active.m-77c9d27d.mantine-Button-root.m-87cf2631.mantine-UnstyledButton-root');
-            ReOpenButton.forEach(button =>
-            {
-                button.click();
-            });
+            if (ReOpenButton) {
+                ReOpenButton.click();
+            }
         }
     }
 
-    function selectCommentBox(event)
-    {
-        if (event.ctrlKey && event.key === '\\')
-        {
+    function selectCommentBox(event) {
+        if (event.ctrlKey && event.key === '\\') {
             // Dark Mode
             const textarea_dark = document.querySelector('.m-8fb7ebe7.mantine-Input-input.mantine-Textarea-input');
-            if (textarea_dark)
+            if (textarea_dark) {
                 textarea_dark.select();
+            }
         }
     }
 
-    function SelectAccounts(event)
-    {
-        if (event.altKey && event.key === 'a')
-        {
+    function SelectAccounts(event) {
+        if (event.altKey && event.key === 'a') {
             const Accounts_button = document.querySelector('a[href="/ng/accounts"].mantine-focus-never.i-root-iHDUF.m-f0824112.mantine-NavLink-root.m-87cf2631.mantine-UnstyledButton-root');
-            if (Accounts_button)
+            if (Accounts_button) {
                 Accounts_button.click();     
+            }
         }
     }
 
-    function search(event)
-    {
-        if (event.altKey && event.key === 's')
-        {
+    function search(event) {
+        if (event.altKey && event.key === 's') {
             // Dark Mode
             const search_button = document.querySelector('button[data-variant="default"][data-size="lg"][aria-label="Search"]');
-            if (search_button)
+            if (search_button) {
                 search_button.click();
+            }
             const input_field = document.querySelector('.ims-sel__input-container.css-n9qnu9')
-            if (input_field)
+            if (input_field) {
                 input_field.focus();
+            }
         }
     }
 
