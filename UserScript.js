8a34ccd3a6370f0317960c3e7f440ceba243ab8e
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2024-05-02 13:34:40 -0400

    Fixed resolveTicket function
    Removed reopen ticket function and fixed resolve ticket function

--- UserScript.js
+++ UserScript.js
@@ -2,7 +2,7 @@
 // @name         IMS+ V2
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Provides functionality to IMS 
+// @description  Provides functionality to IMS
 // @author       JoeyCorbett
 // @author       TylerMong
 // @match        https://ims.stockton.edu/*
@@ -17,18 +17,13 @@
 (function () {
     'use strict'
 
-    function resolveAndReopenTicket(event) {
+    function resolveTicket(event) {
         if (event.ctrlKey && event.key === 'Enter') {
-            const ResolveButton = document.querySelector('button.mantine-focus-never.mantine-active.m-77c9d27d.mantine-Button-root.m-87cf2631.mantine-UnstyledButton-root');
+            console.log("test");
+            const ResolveButton = document.querySelector("#root > div > main > div.i-root-Z4df4 > div > div.m-4081bf90.mantine-Group-root > button:nth-child(4)");
             if (ResolveButton) {
+                console.log("test2");
                 ResolveButton.click();
-            }
-            
-        }
-        if (event.ctrlKey && event.key === 'Enter') {
-            const ReOpenButton = document.querySelectorAll('button.mantine-focus-never.mantine-active.m-77c9d27d.mantine-Button-root.m-87cf2631.mantine-UnstyledButton-root');
-            if (ReOpenButton) {
-                ReOpenButton.click();
             }
         }
     }
@@ -47,7 +42,7 @@
         if (event.altKey && event.key === 'a') {
             const Accounts_button = document.querySelector('a[href="/ng/accounts"].mantine-focus-never.i-root-iHDUF.m-f0824112.mantine-NavLink-root.m-87cf2631.mantine-UnstyledButton-root');
             if (Accounts_button) {
-                Accounts_button.click();     
+                Accounts_button.click();
             }
         }
     }
@@ -66,7 +61,7 @@
         }
     }
 
-    document.addEventListener('keydown', resolveAndReopenTicket);
+    document.addEventListener('keydown', resolveTicket);
     document.addEventListener('keydown', selectCommentBox);
     document.addEventListener('keydown', SelectAccounts);
     document.addEventListener('keydown', search);
